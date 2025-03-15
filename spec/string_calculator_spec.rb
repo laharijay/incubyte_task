7e78d572eda3c7f32c5fc8ef9aa38f922d33420f
laharijay <jay.lahari1999@gmail.com>
2025-03-15 18:32:39 +0530

    Add test for adding  two numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,4 +9,8 @@ RSpec.describe StringCalculator do
   it "returns the number itself for a single number" do
     expect(StringCalculator.new.add("1")).to eq(1)
   end
+
+  it "returns the sum of two numbers" do
+    expect(StringCalculator.new.add("1,2")).to eq(3)
+  end
 end
